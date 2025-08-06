9a34ae25867807e4b596cf987e261ffcf957894e
Bartosz Szymański <bartosz.szymanski@it.integro.pl>
2025-08-06 09:53:15 +0200

    Creating the Course Project

---
+++ TCSA.OOP.LibraryManagementSystem/Program.cs
@@ -0,0 +1,9 @@
+﻿using Spectre.Console;
+
+var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
+
+var choice = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+    .Title("What do you want to do next?")
+    .AddChoices(menuChoices));
+Console.WriteLine($"{choice}");
