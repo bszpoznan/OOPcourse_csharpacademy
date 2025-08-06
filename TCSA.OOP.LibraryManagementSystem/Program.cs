934e70dd5b4d54ddbc7d1eeb8690259f5f8839ee
Bartosz Szymański <bartosz.szymanski@it.integro.pl>
2025-08-06 10:40:23 +0200

    Basic Functionality

--- TCSA.OOP.LibraryManagementSystem/Program.cs
+++ TCSA.OOP.LibraryManagementSystem/Program.cs
@@ -1,9 +1,69 @@
 ﻿using Spectre.Console;
 
 var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
+var books = new List<string>()
+{
+    "Ostatnie życzenie", "Miecz przeznaczenia", "Krew elfów", "Chrzest ognia", "Czas pogardy", "Pani jeziora", "Wieża jaskółki","Sezon burz", "Rozdroże kruków"
+};
 
-var choice = AnsiConsole.Prompt(
+while (true)
+{
+    Console.Clear();
+    var choice = AnsiConsole.Prompt(
     new SelectionPrompt<string>()
-    .Title("What do you want to do next?")
-    .AddChoices(menuChoices));
-Console.WriteLine($"{choice}");
+        .Title("What do you want to do next?")
+        .AddChoices(menuChoices));
+    switch (choice)
+    {
+        case "View Books":
+            AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
+            foreach (var book in books)
+            {
+                AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
+            }
+            AnsiConsole.MarkupLine("Press any key to continue");
+            Console.ReadKey();
+            break;
+        case "Add Book":
+            var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
+            if (books.Contains(title))
+            {
+                AnsiConsole.MarkupLine("[red]This book already exists.[/]");
+            }
+            else
+            {
+                books.Add(title);
+                AnsiConsole.MarkupLine("[green]Book added successfully.[/]");
+            }
+            AnsiConsole.MarkupLine("Press any key to continue.");
+            Console.ReadKey();
+            break;
+        case "Delete Book":
+            if (books.Count == 0)
+            {
+                AnsiConsole.MarkupLine("[red]No books available to delete.[/]");
+                Console.ReadKey();
+                return;
+            }
+            var bookToDelete = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Select a [red]book[/] to delete:")
+                .AddChoices(books)
+            );
+
+            if (books.Remove(bookToDelete))
+            {
+                AnsiConsole.MarkupLine("[red]Book deleted successfully![/]");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Book not found.[/]");
+            }
+            AnsiConsole.MarkupLine("Press any key to continue.");
+            Console.ReadKey();
+
+            break;
+    }
+
+}
+
